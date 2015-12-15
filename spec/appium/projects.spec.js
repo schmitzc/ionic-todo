91c860c777bbd82a0ab452c0e35e0fc9e74b46bb
Cassie Schmitz <schmitzc@gmail.com>
2015-12-14 19:37:12 -0800

    Add appium spec for creating task

--- spec/appium/projects.spec.js
+++ spec/appium/projects.spec.js
@@ -4,15 +4,21 @@ var path = require('path');
 
 require(path.resolve('spec/appium/setup.js'))(function(driver) {
   it('should create the initial project', function () {
+    var project = 'Tests';
     return driver
-      .elementByCss('.project-title').type('Tests')
+      .elementByCss('.project-title').type(project)
       .elementByCss('.button-positive').click()
       .elementByCss('.active-project-title').text()
-      .should.become('Tests');
+      .should.become(project);
   });
 
   it('should create a task', function() {
+    var task = 'Write tests';
     return driver
-      .elementByCss('.open-new-task').click();
+      .elementByCss('.open-new-task').click()
+      .elementByCss('.task-title').type(task)
+      .elementByCss('.create-task').click()
+      .elementByCss('.item').text()
+      .should.become(task);
   });
 });
