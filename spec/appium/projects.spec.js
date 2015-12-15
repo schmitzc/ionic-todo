ef9f63d9b92e5f89231a0959a64910279a2ea2c7
Cassie Schmitz <schmitzc@gmail.com>
2015-12-15 10:06:04 -0800

    Add appium spec to select a project

--- spec/appium/projects.spec.js
+++ spec/appium/projects.spec.js
@@ -4,47 +4,52 @@ var path = require('path');
 var wd = require('wd');
 
 require(path.resolve('spec/appium/setup.js'))(function(driver) {
+  var initialProjectTitle = 'Tests';
   var activeProjectSelector = '.active-project-title';
   var openNewTaskSelector = '.open-new-task';
-  var newTaskModal = '.new-task-modal';
+  var toggleProjectsSelector = '.toggle-projects-menu';
 
   it('should create the initial project', function () {
-    var project = 'Tests';
-
     return driver
-      .elementByCss('.project-title').type(project)
+      .elementByCss('.project-title').type(initialProjectTitle)
       .elementByCss('.button-positive').click()
       .elementByCss(activeProjectSelector).text()
-      .should.become(project);
+      .should.become(initialProjectTitle);
   });
 
   it('should create a task', function() {
     var task = 'Write tests';
 
     return driver
-      .elementByCss(openNewTaskSelector).click()
-      .elementByCss('.task-title').type(task)
+      .waitForElementByCss(openNewTaskSelector, wd.asserters.isDisplayed, 2000, function(err, el) {
+        el.click();
+      })
+      .waitForElementByCss('.task-title', wd.asserters.isDisplayed, 2000, function(err, el) {
+        el.type(task);
+      })
       .elementByCss('.create-task').click()
       .elementsByCss('.task').then(function(elements) {
         elements.length.should.equal(1);
         elements[0].text().should.become(task);
-        return elements;
       });
   });
 
   it('should close the new task modal', function() {
     return driver
-      .elementByCss(openNewTaskSelector).click()
-      .waitForElementByCss(newTaskModal, wd.asserters.isDisplayed, 2000)
-      .elementByCss('.close-new-task').click()
-      .waitForElementByCss(newTaskModal, wd.asserters.isNotDisplayed, 2000);
+      .waitForElementByCss(openNewTaskSelector, wd.asserters.isDisplayed, 2000, function(err, el) {
+        el.click();
+      })
+      .waitForElementByCss('.close-new-task', wd.asserters.isDisplayed, 2000, function(err, el) {
+        el.click();
+      })
+      .waitForElementByCss(activeProjectSelector, wd.asserters.isDisplayed, 2000);
   });
 
   it('should create a new project', function() {
     var project = 'Release';
 
     return driver
-      .elementByCss('.toggle-projects-menu').click()
+      .elementByCss(toggleProjectsSelector).click()
       .elementByCss('.open-new-project').click()
       .elementByCss('.project-title').type(project)
       .elementByCss('.create-project').click()
@@ -53,7 +58,19 @@ require(path.resolve('spec/appium/setup.js'))(function(driver) {
       .elementsByCss('.project').then(function(elements) {
         elements.length.should.equal(2);
         elements[1].text().should.become(project);
-        return elements;
+      });
+  });
+
+  it('should select a project', function() {
+    return driver
+      .elementByCss(toggleProjectsSelector).click()
+      .elementsByCss('.project').then(function(elements) {
+        elements[0].click();
+      })
+      .elementByCss(activeProjectSelector).text()
+      .should.become(initialProjectTitle)
+      .elementsByCss('.task').then(function(elements) {
+        elements.length.should.equal(1);
       });
   });
 });
